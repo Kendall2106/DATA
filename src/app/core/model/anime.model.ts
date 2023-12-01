a23b2d226f8da41076c779b4b51a8c8f19cd3c0a
Practicante3 <practicante3@cnet.co.cr>
2023-12-01 05:51:56 -0600

    a ver 3

--- src/app/core/model/anime.model.ts
+++ src/app/core/model/anime.model.ts
@@ -1,5 +1,12 @@
 export interface Anime {
+  id: number;
   name: string;
+  date: Date;
+  score: number;
+  type: string;
+  numTemp: number;
+  image: string;
+  visible: boolean;
 }
 
 export class Anime implements Anime {
--- src/app/pages/workplace/workplace.component.ts
+++ src/app/pages/workplace/workplace.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { NavigationStart, Router } from '@angular/router';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { Anime } from 'src/app/core/model/anime.model';
 
 import { Movie } from 'src/app/core/model/movie.model';
+import { AnimeService } from 'src/app/core/service/anime.service';
 import { DataService } from 'src/app/core/service/data.service';
 import { MovieService } from 'src/app/core/service/movie.service';
 
@@ -11,14 +13,19 @@ import { MovieService } from 'src/app/core/service/movie.service';
   styleUrls: ['./workplace.component.css'],
 })
 export class WorkplaceComponent implements OnInit {
+  data: any[] = [];
   movies: Movie[] = [];
+  animes: Anime[] = [];
   message: string = '';
-  opTipos: string[] = [];
+  opTipos: string[][] = [];
   opAnios: string[] = [];
   opCalificacion: string[] = [];
   calificacionSelec: any;
-  constructor(private movieService: MovieService, private router: Router, public dataService: DataService) {
-      this.opTipos= ["Accion","Terror","Comedia"];
+  constructor(private movieService: MovieService, private animeService: AnimeService,private router: Router, public dataService: DataService) {
+    this.opTipos = [
+      ["Accion", "Terror", "Comedia", "Animacion", "Musical", "Romance", "Triller"],
+      ["lightcoral", "gray", "blue", "green", "yellow", "Pink", "White"] // Colores correspondientes
+    ];
       this.opAnios= ["2020","2021","2022"];
       this.opCalificacion = ["0","1","2","3","4","5"];
   }
@@ -28,14 +35,30 @@ export class WorkplaceComponent implements OnInit {
       this.router.navigate(['/']);
     }
 
-    this.message = this.dataService.typeData;
+    this.router.events.subscribe(event => {
+      
+        this.message = this.dataService.typeData;
+        this.loadData();
+   
+    });
+  }
 
-    if(this.message=="Peliculas"){
+  loadData() {
+    if (this.message === 'Peliculas') {
       this.movieService.getMovies().subscribe(movies => {
-        this.movies = movies.sort((a, b) => b.id - a.id); //Orden Desc
+        this.data = movies.sort((a, b) => b.id - a.id);
+      });
+    } else if (this.message === 'Animes') {
+      this.animeService.getAnimes().subscribe(animes => {
+        this.data = animes.sort((a, b) => b.id - a.id);
       });
     }
-    
+    // Añade lógica para otros casos si es necesario
+  }
+
+  getColorByType(type: string): string {
+    const index = this.opTipos[0].indexOf(type);
+    return index !== -1 ? this.opTipos[1][index] : 'greenyellow'; // Color por defecto para tipos desconocidos
   }
 
 
