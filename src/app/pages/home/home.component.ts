59edbb4fa39f73f7c1742a4c6adaa7dac61cb618
Practicante3 <practicante3@cnet.co.cr>
2023-12-04 01:36:21 -0600

    a ver

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -128,15 +128,15 @@ export class HomeComponent implements OnInit{
 
 
  onSelect(data: any): void {
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+    //console.log('Item clicked', JSON.parse(JSON.stringify(data)));
   }
 
   onActivate(data: any): void {
-    console.log('Activate', JSON.parse(JSON.stringify(data)));
+    //console.log('Activate', JSON.parse(JSON.stringify(data)));
   }
 
   onDeactivate(data: any): void {
-    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+    //console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 
