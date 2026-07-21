3ccd4ff5c963b00dd815316eac0367e4199fca73
Ken2106 <83435540+Kendall2106@users.noreply.github.com>
2026-07-21 02:34:58 -0600

    books api

--- src/app/core/service/apiBook.service.ts
+++ src/app/core/service/apiBook.service.ts
@@ -8,23 +8,24 @@ import { Observable } from 'rxjs';
 })
 export class ApiBookService {
   private API_URL = 'https://www.googleapis.com/books/v1/volumes';
+private API_KEY = 'AIzaSyDroAOHJga-Luumr1O7RZjOdppHo9DS7Sk';
 
-  constructor(private http: HttpClient) {}
+constructor(private http: HttpClient) {}
 
-  getBooks(startIndex: number, maxResults: number): Observable<any> {
-    return this.http.get(`${this.API_URL}?q=""+subject:fantasy&startIndex=${startIndex}&maxResults=24&printType=books`);
-  }
+getBooks(startIndex: number, maxResults: number): Observable<any> {
+  const url = `${this.API_URL}?q=subject:fantasy&startIndex=${startIndex}&maxResults=${maxResults}&printType=books&key=${this.API_KEY}`;
+  return this.http.get(url);
+}
 
-  getBooksByName(name: string,startIndex: number) {
-    const url = `${this.API_URL}?q=${name}&startIndex=${startIndex}&maxResults=24&printType=books`;
-    return this.http.get(url);
-  }
+getBooksByName(name: string, startIndex: number): Observable<any> {
+  const url = `${this.API_URL}?q=${encodeURIComponent(name)}&startIndex=${startIndex}&maxResults=24&printType=books&key=${this.API_KEY}`;
+  return this.http.get(url);
+}
 
-  getBooksByNew() {
-    const url = `${this.API_URL}?q=""+subject:fantasy&maxResults=24&printType=books&orderBy=newest`;
-    return this.http.get(url);
-  }
-
+getBooksByNew(): Observable<any> {
+  const url = `${this.API_URL}?q=subject:fantasy&maxResults=24&printType=books&orderBy=newest&key=${this.API_KEY}`;
+  return this.http.get(url);
+}
   
 
 
